726e557cc52af7a316108221032b4d7e331c5fb8
Ginessa Monaco <ginessalynnmonaco@gmail.com>
2024-09-12 21:39:37 -0400

    delete file

--- day 001 - band name generator/bandnamegen.py
+++
@@ -1,6 +0,0 @@
-print("WELCOME TO THE BAND NAME GENERATOR!")
-
-city = input("What is your favorite shape?\n")
-name = input("What is your first pets name?\n")
-
-print("Your band name should be", city, name, ":D")
